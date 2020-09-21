8af18b520cbdcbb207b6bee62bdcb38ed2d20915
yuchenliu <yuchenliu@synology.com>
2020-09-21 16:27:01 +0800

    Refact Adjust to fit Synology coding style and get rid of redundant function

--- synonetfilter.c
+++ synonetfilter.c
@@ -13,27 +13,12 @@
 static struct nf_hook_ops *nfhoIN = NULL;
 static struct nf_hook_ops *nfhoOUT = NULL;
 
-static unsigned int filterSMB2Payload(unsigned char *data, unsigned int len)
-{
-    size_t i = 0;
-    unsigned char distill = 0xff;
-    for (i = 0; i < len; i++) {
-        distill = distill & data[i];
-    }
-    
-    if (distill == 0xff) {
-        return 0;
-    }
-    else {
-        return 1;
-    }
-}
-
-static void printSignificantPayload(unsigned char *data, unsigned int len, unsigned int range)
+static int printSignificantPayload(unsigned char *data, unsigned int len, unsigned int range)
 {
     size_t i = 0;
     size_t j = 0;
     unsigned char distill = 0xff;
+    int ret = 0;
     for (i = 0; i < len; i++) {
         distill = distill & data[i];
         if (distill != 0xff) {
@@ -50,14 +35,18 @@ static void printSignificantPayload(unsigned char *data, unsigned int len, unsig
             else {
                 printk(KERN_NOTICE "=>%x\n", data[i]);
             }
+            ret = 1;
             break;
         }
     }
+
+    return ret;
 }
 
 static unsigned int getSMB2Header(struct sk_buff *skb, struct SMB2_HEADER *smbhdr)
 {    
     size_t offset = IP_HDR_SIZE + TCP_HDR_SIZE + NET_BIOS_SIZE;
+    unsigned int ret = 0;
     if (!skb || !smbhdr) {
         goto fail;
     }
@@ -70,9 +59,9 @@ static unsigned int getSMB2Header(struct sk_buff *skb, struct SMB2_HEADER *smbhd
         goto fail;
     }
 
-    return 1;
+    ret = 1;
 fail:
-    return 0;
+    return ret;
 }
 
 static unsigned int hfuncIN(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
@@ -105,8 +94,7 @@ static unsigned int hfuncIN(void *priv, struct sk_buff *skb, const struct nf_hoo
             if (IS_WRITE(be16_to_cpu(smbhdr.command)) && IS_REQUEST(be32_to_cpu(smbhdr.flags))) {
                 printk(KERN_NOTICE "write request\n");
 
-                if (filterSMB2Payload(&skb->data[TOTAL_WRITE_REQUEST_HDR_SIZE], skb->len - TOTAL_WRITE_REQUEST_HDR_SIZE)) {
-                    printSignificantPayload(&skb->data[TOTAL_WRITE_REQUEST_HDR_SIZE], skb->len - TOTAL_WRITE_REQUEST_HDR_SIZE, 4);
+                if (printSignificantPayload(&skb->data[TOTAL_WRITE_REQUEST_HDR_SIZE], skb->len - TOTAL_WRITE_REQUEST_HDR_SIZE, 4)) {
                     tail = skb_tail_pointer(skb);
                     for (iter = skb->data; iter != tail; iter++) {
                         printk(KERN_NOTICE "%x", *iter);
@@ -156,8 +144,7 @@ static unsigned int hfuncOUT(void *priv, struct sk_buff *skb, const struct nf_ho
             if (IS_READ(be16_to_cpu(smbhdr.command)) && IS_RESPONSE(be32_to_cpu(smbhdr.flags))) {
                 printk(KERN_NOTICE "read response\n");
 
-                if (filterSMB2Payload(&skb->data[TOTAL_READ_RESPONSE_HDR_SIZE], skb->len - TOTAL_READ_RESPONSE_HDR_SIZE)) {
-                    printSignificantPayload(&skb->data[TOTAL_READ_RESPONSE_HDR_SIZE], skb->len - TOTAL_READ_RESPONSE_HDR_SIZE, 4);
+                if (printSignificantPayload(&skb->data[TOTAL_READ_RESPONSE_HDR_SIZE], skb->len - TOTAL_READ_RESPONSE_HDR_SIZE, 4)) {
                     tail = skb_tail_pointer(skb);
                     for (iter = skb->data; iter != tail; iter++) {
                         printk(KERN_NOTICE "%x", *iter);
@@ -180,13 +167,20 @@ forward:
 static int __init SYNONETFILTER_init(void)
 {
     nfhoIN = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
-    nfhoOUT = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+    if (nfhoIN == NULL) {
+        goto fail;
+    }
     
     /* Initialize netfilter hook */
     nfhoIN->hook = (nf_hookfn*)hfuncIN;     /* hook function */
     nfhoIN->hooknum = NF_INET_LOCAL_IN;    /* received packets */
     nfhoIN->pf = PF_INET;    /* IPv4 */
     nfhoIN->priority = NF_IP_PRI_FIRST;    /* max hook priority */
+
+    nfhoOUT = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+    if (nfhoOUT == NULL) {
+        goto fail;
+    }
     
     nfhoOUT->hook = (nf_hookfn*)hfuncOUT;     /* hook function */
     nfhoOUT->hooknum = NF_INET_LOCAL_OUT;    /* received packets */
@@ -196,15 +190,20 @@ static int __init SYNONETFILTER_init(void)
     nf_register_net_hook(&init_net, nfhoIN);
     nf_register_net_hook(&init_net, nfhoOUT);
 
+fail:
     return 0;
 }
 
 static void __exit SYNONETFILTER_exit(void)
 {
-    nf_unregister_net_hook(&init_net, nfhoIN);
-    kfree(nfhoIN);
-    nf_unregister_net_hook(&init_net, nfhoOUT);
-    kfree(nfhoOUT);
+    if (nfhoIN != NULL) {
+        nf_unregister_net_hook(&init_net, nfhoIN);
+        kfree(nfhoIN);
+    }
+    if (nfhoOUT != NULL) {
+        nf_unregister_net_hook(&init_net, nfhoOUT);
+        kfree(nfhoOUT);
+    }
 }
 
 module_init(SYNONETFILTER_init);
